dc11f56ee0e2357ed0e36497b47d50e6877ae2e3
lizuoyang <lizuoyangsina@qq.com>
2022-06-20 15:56:52 +0800

    更改作者相关信息

--- tests/unit/utils/validate.spec.js
+++ tests/unit/utils/validate.spec.js
@@ -7,9 +7,9 @@ describe('Utils:validate', () => {
     expect(validUsername('xxxx')).toBe(false)
   })
   it('isExternal', () => {
-    expect(isExternal('https://github.com/PanJiaChen/vue-element-admin')).toBe(true)
-    expect(isExternal('http://github.com/PanJiaChen/vue-element-admin')).toBe(true)
-    expect(isExternal('github.com/PanJiaChen/vue-element-admin')).toBe(false)
+    expect(isExternal('https://gitee.com/lizuoyang/lzy-cloud-front')).toBe(true)
+    expect(isExternal('https://gitee.com/lizuoyang/lzy-cloud-front')).toBe(true)
+    expect(isExternal('https://gitee.com/lizuoyang/lzy-cloud-front')).toBe(false)
     expect(isExternal('/dashboard')).toBe(false)
     expect(isExternal('./dashboard')).toBe(false)
     expect(isExternal('dashboard')).toBe(false)
